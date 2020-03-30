5d07bd8dadf974b058f48eb2accae8a0c321e22a
Pierre-Antoine <pierre-antoine.burdet@hardis.fr>
2020-03-30 15:40:27 +0200

    Remove /target/ tracking

--- rusty-http3/target/debug/deps/rusty_http3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\deps\rusty_http3.exe: src\main.rs
-
-C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\deps\rusty_http3.d: src\main.rs
-
-src\main.rs:
--- rusty-http3/target/debug/rusty-http3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\rusty-http3.exe: C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\src\main.rs
