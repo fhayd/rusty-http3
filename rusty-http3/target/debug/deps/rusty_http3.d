5e3f2ca4d055aada4be006385fac9a0f4cf31e71
Pierre-Antoine <pierre-antoine.burdet@hardis.fr>
2020-03-30 09:41:39 +0200

    Init cargo project

---
+++ rusty-http3/target/debug/deps/rusty_http3.d
@@ -0,0 +1,5 @@
+C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\deps\rusty_http3.exe: src\main.rs
+
+C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\deps\rusty_http3.d: src\main.rs
+
+src\main.rs:
---
+++ rusty-http3/target/debug/rusty-http3.d
@@ -0,0 +1,1 @@
+C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\target\debug\rusty-http3.exe: C:\Users\MachoNacho\source\repos\rusty-http3\rusty-http3\src\main.rs
